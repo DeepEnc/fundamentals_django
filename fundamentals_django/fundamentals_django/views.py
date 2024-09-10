4692f28308d08d63d1772ee35c439b94f3a04b19
deepenc <sunilojha525@gmail.com>
2024-09-10 18:43:09 +0545

    feat: add the css

--- fundamentals_django/fundamentals_django/views.py
+++ fundamentals_django/fundamentals_django/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("This is the Home Page")
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("This is the About Page")
