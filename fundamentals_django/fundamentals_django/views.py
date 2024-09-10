78e05df94fd52f59a09c1ab52871c807cfdf0968
deepenc <sunilojha525@gmail.com>
2024-09-10 18:21:50 +0545

    feat: add the templates

--- fundamentals_django/fundamentals_django/views.py
+++ fundamentals_django/fundamentals_django/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is the Home Page")
+    # return HttpResponse("This is the Home Page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("This is the About Page")
