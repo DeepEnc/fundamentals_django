b73b2b364780589d02a6950de02640fe20a4712e
deepenc <sunilojha525@gmail.com>
2024-09-11 15:17:10 +0545

    feat: add browser reload config

--- fundamentals_django/fundamentals_django/settings.py
+++ fundamentals_django/fundamentals_django/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'fundamental_app',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'fundamentals_django.urls'
