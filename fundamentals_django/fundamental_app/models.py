41b72cfc080539829c40914c3227c4967fb8c73e
deepenc <sunilojha525@gmail.com>
2024-09-11 22:12:26 +0545

    feat: add basic config to models

--- fundamentals_django/fundamental_app/models.py
+++ fundamentals_django/fundamental_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class AppVariety(models.Model):
+    APP_TYPE_CHOICE = [
+        ('social', 'agriculture'),
+        ('tech', 'social'),
+        ('life', 'eco'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='fundamental_app/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=6, choices=APP_TYPE_CHOICE)
