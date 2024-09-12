58aa10a8a2237298365a23d9aa11716c68477295
deepenc <sunilojha525@gmail.com>
2024-09-12 14:54:19 +0545

    feat: connect models to the front end

--- fundamentals_django/fundamental_app/views.py
+++ fundamentals_django/fundamental_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import AppVariety
 
 def app(request):
-    return render(request, 'fundamental_app/app.html')
+    apps = AppVariety.objects.all()
+    return render(request, 'fundamental_app/app.html', {'apps': apps})
