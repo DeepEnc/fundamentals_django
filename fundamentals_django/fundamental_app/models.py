2c17b265b6523ccc46f31b2c6f044d918cabb321
deepenc <sunilojha525@gmail.com>
2024-09-12 15:01:47 +0545

    feat: add a field to the model and migrate

--- fundamentals_django/fundamental_app/models.py
+++ fundamentals_django/fundamental_app/models.py
@@ -11,7 +11,7 @@ class AppVariety(models.Model):
     image = models.ImageField(upload_to='apps/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=APP_TYPE_CHOICE)
-
+    description = models.TextField(default='')
 
 def __str__(self):
     return self.name
