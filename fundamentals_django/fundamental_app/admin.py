357dbd5edaad65198f520d91fff08737c4d5d8e9
deepenc <sunilojha525@gmail.com>
2024-09-14 22:50:53 +0545

    feat: add the admin config

--- fundamentals_django/fundamental_app/admin.py
+++ fundamentals_django/fundamental_app/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from . models import AppVariety
+from .models import AppVariety
+from .models import AppReview
+from .models import AppStore
+from .models import AppCertificate
 
-admin.site.register(AppVariety)
+class AppReviewInLine(admin.TabularInline):
+    model = AppReview
+    extra = 2
+
+class AppVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [AppReviewInLine]
+
+class AppStoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('app_varieties', )
+
+class AppCertificateAdmin(admin.ModelAdmin):
+    list_display = ('app', 'certificates_number')
+
+admin.site.register(AppVariety, AppVarietyAdmin)
+admin.site.register(AppStore, AppStoreAdmin)
+admin.site.register(AppCertificate, AppCertificateAdmin)
